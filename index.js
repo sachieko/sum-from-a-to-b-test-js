f75d444e7e5cadbf505768184c49d622986c19fd
sachieko <kirazetsubou@gmail.com>
2022-07-31 00:54:44 -0600

    Implemented function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = function(fromN, toN) {
+  return fromN === toN ? toN : fromN + sum(fromN + 1, toN);
+};
 
 module.exports = sum;
